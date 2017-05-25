4e47293c1ad5857487039c9ef50ef23339a4dc6d
DreamPaths Projekt <dream.paths.projekt@gmail.com>
2017-05-26 00:01:06 +0300

    Add save method in VolunteerDAO class

--- src/org/ffcc/communityVessells/app/dao/VolunteerDAO.java
+++ src/org/ffcc/communityVessells/app/dao/VolunteerDAO.java
@@ -43,7 +43,7 @@ public class VolunteerDAO {
 			return volunteerList;
 		}
 		catch(SQLException sqlE){
-			throw new Exception("An error occured while getting volunteers from database: " + sqlE.getMessage());
+			throw new Exception("An error occured while getting users from database: " + sqlE.getMessage());
 		}
 		finally {
 			try{
@@ -56,7 +56,39 @@ public class VolunteerDAO {
 		}
 	}
 	 public void saveVolunteer(Volunteer volunteer) throws Exception{
-		 
+			Connection con = null;
+			String sqlcreate = "INSERT INTO  volunteer (promises, email, password,username,firstName,lastName,avatar) VALUES (? , ? , ? , ? , ? , ? , ?);";
+			DB db = new DB();
+			
+			try{
+				db.open();
+				con = db.getConnection();
+				
+				PreparedStatement insertst = con.prepareStatement(sqlcreate);
+				
+				insertst.setInt(1, volunteer.getPromises());
+				insertst.setString(2, volunteer.getEmail());
+				insertst.setString(3, volunteer.getPassword());
+				insertst.setString(4, volunteer.getUsername());
+				insertst.setString(5, volunteer.getFirstName());
+				insertst.setString(6, volunteer.getLastName());
+				insertst.setBlob(7, volunteer.getAvatar());
+				
+				insertst.executeUpdate();
+				insertst.close();
+				
+			}catch (SQLException e) {
+
+				throw new Exception("Volunteer with ID: " + volunteer.getUserID() + " already exists");
+			}catch (Exception e){
+				throw new Exception("An error occured while inserting user to database: " + e.getMessage());
+			}finally{
+				try{
+					db.close();
+				}catch (Exception e){
+					e.printStackTrace();
+				}
+			}
 	 }
 	
 	
